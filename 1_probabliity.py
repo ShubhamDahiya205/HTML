4544998bac11f54e6135cdd1b7d2282d46a47c5c
Shubham Dahiya <shubhnaam205@gmail.com>
2024-05-15 08:51:49 +0530

    Made user input for upper limit and added option to use it multiple times

--- 1_probabliity.py
+++ 1_probabliity.py
@@ -1,10 +1,14 @@
 import random as rand
-probability = {}
-for i in range(1,101):
-    probability[i] = 0
-for i in range(1,101):
-    number = rand.randint(1,100)
-    probability[number] = probability[number] + 1
-    print(str(number), end= " ")
-print("")
-print(probability)
+while True:
+    upper = int(input("Enter upper limit"))
+    probability = {}
+    for i in range(1,upper+1):
+        probability[i] = 0
+    for i in range(1,upper+1):
+        number = rand.randint(1,upper+1)
+        probability[number] = probability[number] + 1
+    print(probability)
+    user_choice = input("Enter \"exit\" to exit")
+    if user_choice == "exit":
+        break
+print("Thanks")
