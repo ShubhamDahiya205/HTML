dc30feaac8e6a3b3aa1915a05e3cb952f2a61200
Shubham Dahiya <shubhnaam205@gmail.com>
2024-05-15 09:30:26 +0530

    Errors fixed: randint range and added except for errors

--- 1_probabliity.py
+++ 1_probabliity.py
@@ -1,14 +1,23 @@
 import random as rand
+print("Check How random the \"random\" module really is.")
 while True:
-    upper = int(input("Enter upper limit"))
-    probability = {}
-    for i in range(1,upper+1):
-        probability[i] = 0
-    for i in range(1,upper+1):
-        number = rand.randint(1,upper+1)
-        probability[number] = probability[number] + 1
-    print(probability)
-    user_choice = input("Enter \"exit\" to exit")
-    if user_choice == "exit":
-        break
+    try:
+        upper = int(input("Enter upper limit: "))
+    except:
+        print("Enter a number!")
+        continue  
+    if upper >1 :
+        probability = {}
+        for i in range(1,upper+1):
+            probability[i] = 0
+        for i in range(1,upper+1):
+            number = rand.randint(1,upper)
+            probability[number] = probability[number] + 1
+        print(probability)
+        user_choice = input("Enter \"exit\" to exit: ")
+        if user_choice == "exit":
+            break    
+    else:
+        print("Number should be > 1")
+        pass
 print("Thanks")
